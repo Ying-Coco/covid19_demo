cfd71512e35bbc7314c5896b7883655a7e1a00f2
Ying <ywen1306@csu.fullerton.edu>
2020-05-27 22:01:55 -0700

    fixes

--- src/components/component/CountryPicker.jsx
+++ src/components/component/CountryPicker.jsx
@@ -19,7 +19,7 @@ const CountryPicker = ({handleCountryChange}) => {
   return (
     <FormControl className={styles.FormControl}>
       <NativeSelect defaultValue="" style={{width:"300px"}} onChange={(e) => handleCountryChange(e.target.value)}>
-        <option value="" >Global</option>
+        <option value="" style={{textalign:"center"}}>Global</option>
         {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
       </NativeSelect>
     </FormControl>
